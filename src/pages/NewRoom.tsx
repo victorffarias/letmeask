58339a4f88ffc51cc52c894d9ff1a48e10f33d3b
victor farias <victor.f.farias@itamaraty.gov.br>
2021-06-24 23:50:31 -0300

    [ADD] Criação da parte inicial de criação de perguntas com firebase

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -6,10 +8,36 @@ import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
+
 
 
 export function NewRoom() {
-    // const {user} = useContext(AuthContext)
+    const {user} = useAuth();
+    const history = useHistory();
+
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+        if(newRoom.trim() === ''){
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        });
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+
+
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -21,10 +49,12 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="LetMeAsk" />
                     <h2>Criar uma nova sala.</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder="Nome da Sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">Criar sala</Button>
                     </form>
